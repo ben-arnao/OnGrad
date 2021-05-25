3e76c0acb433c059828bf1aeb2898605fedc1636
Ben Arnao <arnaoben@gmail.com>
2021-05-25 10:55:32 -0400

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -12,17 +12,17 @@ def train(
         train_samples,
         test_samples,
         output_size,
-        mom=0.99,
-        grad_noise=1e-2,
+        mom=0.998,
+        grad_noise=2e-2,
         weight_decay=0.1,
-        reduce_lr_patience=25,
+        reduce_lr_patience=50,
         base_lr=1e-5,
         step_clip_factor=5,
         score_delta_warmup_scale=1,
         n_layers=4,
         kernel_size=200,
         activation='mish',
-        batch_size=32768):
+        predict_batch_size=32768):
 
     # make model
     model_arch = [kernel_size for _ in range(n_layers)]
@@ -38,9 +38,9 @@ def train(
     model.compile()
 
     # get baselines
-    train_preds = model.predict(train_samples, batch_size=batch_size)
+    train_preds = model.predict(train_samples, batch_size=predict_batch_size)
     best_train_score = get_score(train_preds)
-    test_preds = model.predict(test_samples, batch_size=batch_size)
+    test_preds = model.predict(test_samples, batch_size=predict_batch_size)
     best_test_score = get_score(test_preds)
 
     # setup score history tracking
@@ -50,7 +50,7 @@ def train(
     # get param count
     num_params = len(get_model_params(model))
 
-    # grad tracking vars
+    # gradient estimate
     grad = np.zeros(num_params, dtype=np.float32)
 
     # prepare vars
@@ -67,37 +67,42 @@ def train(
         set_model_params(model, theta)
         return score
 
-    def add_grad_estimate(grad):
+    def add_sample_to_grad_estimate(grad):
         # generate noise
         v = np.random.normal(scale=grad_noise, size=num_params)
+
+        # we do not want noise too small as this will cause misleadingly large gradient estimates
         min_noise = 1e-3
         while np.any(np.abs(v) < min_noise):
             v = np.where(np.abs(v) < min_noise, np.random.normal(scale=grad_noise, size=num_params), v)
 
         # calculate negative/positive noise scores
-        pos_rew = get_step_score(model, v, train_samples, batch_size)
-        neg_rew = get_step_score(model, -v, train_samples, batch_size)
+        pos_rew = get_step_score(model, v, train_samples, predict_batch_size)
+        neg_rew = get_step_score(model, -v, train_samples, predict_batch_size)
 
         if pos_rew != 0 and neg_rew != 0:
             # add to gradient estimate
+            # percent improvement / distance between sample inputs
             g = (pos_rew / neg_rew - 1) / (v * 2)
             return grad * mom + g * (1 - mom)
         return grad
 
     while True:
-        grad = add_grad_estimate(grad)
+        # always add at least one sample to estimate each iteration
+        grad = add_sample_to_grad_estimate(grad)
 
+        # calculate step
         step = grad * lr
 
-        # clip step size to try and not exceed estimation noise
+        # clip step size to try and not exceed the noise used for gradient estimation
         step = np.clip(step, -grad_noise * step_clip_factor, grad_noise * step_clip_factor)
 
-        # calculate average step magnitude
+        # calculate average step magnitude for use in determining weight decay factor
         step_mag = np.mean(np.absolute(step))
 
         # get step score for train and test
-        new_train_score = get_step_score(model, step, train_samples, batch_size)
-        test_score = get_step_score(model, step, test_samples, batch_size)
+        new_train_score = get_step_score(model, step, train_samples, predict_batch_size)
+        test_score = get_step_score(model, step, test_samples, predict_batch_size)
 
         # take step
         set_model_params(model, get_model_params(model) + step)
@@ -106,15 +111,19 @@ def train(
         # (this is why weight decay should be a higher value by default)
         set_model_params(model, get_model_params(model) * (1 - weight_decay * step_mag))
 
-        # calculate score delta
+        # If score drops, calculate additional samples for gradient estimate.
+        # The larger the drop, the more samples will be calculated and the more
+        # accurate and "up to date" the estimate will be.
         if new_train_score < train_score:
-            drop = abs(new_train_score / train_score - 1)
-            drop_warmup = 1 + int(round(drop * score_delta_warmup_scale * (1 / (1 - mom))))
-            for x in range(drop_warmup):
-                grad = add_grad_estimate(grad)
-                print(x, drop_warmup, np.mean(np.abs(grad)), np.max(np.abs(grad)))
+            drop_percent = abs(new_train_score / train_score - 1)
 
-        # set score
+            # calculate num samples based on drop percentage, momentum (higher mom, more samples), and
+            # a user-supplied coefficient
+            recalibration_samples = 1 + int(round(drop_percent * score_delta_warmup_scale * (1 / (1 - mom))))
+            for _ in range(recalibration_samples):
+                grad = add_sample_to_grad_estimate(grad)
+
+        # set score after calculating score delta
         train_score = new_train_score
 
         # track optimization
@@ -139,9 +148,11 @@ def train(
             iters_since_last_best += 1
             lr_reduce_wait += 1
 
+            # reduce lr
             if lr_reduce_wait >= reduce_lr_patience:
                 lr /= 2
                 lr_reduce_wait = 0
-
+            
+            # end training if there is no improvement after lowering LR
             if iters_since_last_best >= reduce_lr_patience * 2:
                 return history
