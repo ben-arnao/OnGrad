b13aea620d946a3cc8915931dca7a88016db119b
Ben Arnao <arnaoben@gmail.com>
2021-05-28 21:06:18 -0400

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -21,7 +21,7 @@ def train(
 
         weight_decay_coeff=0.1,  # weight decay = mean absolute step size * 'weight_decay_coeff'
 
-        step_norm_factor=1e-1,  # step is always a factor of the noise size
+        step_norm_factor=1,  # step is always a factor of the noise size
 
         step_clip_factor=5,  # we do not want to take steps that exceed the size of noise used for grad estimate
         # recommended 3-10
@@ -34,9 +34,8 @@ def train(
         base_est_iters=1  # base amount of samples to add to gradient estimate per step
 
 ):
-    # get baselines
-    score = get_episode_score(model)
-    best_score = score
+    # get baseline
+    best_score = get_episode_score(model)
 
     # setup score history tracking
     score_history = []
@@ -72,19 +71,21 @@ def train(
             score_diff = abs(pos_rew - neg_rew)
             grad = np.where(pos_rew > neg_rew, grad + v * score_diff, grad - v * score_diff)
 
-        # decay gradient est
+        # decay gradient est to fresh estimate over time
         grad *= momentum
 
         return grad
 
     while True:
+        
+        # base samples each iteration used to estimate gradient
         for _ in range(base_est_iters):
             grad = add_sample_to_grad_estimate(grad)
 
-        # calculate step
+        # <<<< calculate step >>>>
         # rescale grad to mean of 1 because with grad we only care about values relative to another.
         # if we don't rescale, step_size can be affected by the scale change of score, which is irrelevant to gradient
-        # then make step size a factor of noise
+        # then make step size a factor of noise used for estimating gradient
         grad_rescaled = grad / np.mean(np.abs(grad))
         step = grad_rescaled * ((grad_noise * step_norm_factor) / np.mean(np.abs(grad_rescaled)))
 
@@ -101,21 +102,16 @@ def train(
         set_model_params(model, get_model_params(model) + step)
 
         # decay weights where weight decay value is modified by the step magnitude
-        # (this is why weight decay should be a higher value by default)
         set_model_params(model, get_model_params(model) * (1 - weight_decay_coeff * step_mag))
 
-        # If score drops, calculate additional samples for gradient estimate.
-        # The larger the drop, the more samples will be calculated and the more
-        # accurate and "up to date" the estimate will be.
+        # calculate num recalibration samples based on score drop percentage
         if new_score < score:
 
             if score < 0 or new_score < 0:
                 raise NotImplementedError('Does not currently handle negative scores. consider adding a baseline and'
                                           'clipping anything under to 0')
+            
             drop_percent = abs(new_score / score - 1)
-
-            # calculate num samples based on drop percentage, momentum (higher mom, more samples), and
-            # a user-supplied coefficient
             recalibration_samples = 1 + int(round(drop_percent * score_delta_warmup_scale))
             for _ in range(recalibration_samples):
                 grad = add_sample_to_grad_estimate(grad)
@@ -139,6 +135,10 @@ def train(
 
             # reduce lr/grad decay
             if lr_reduce_wait >= lr_patience:
+                
+                # lower the LR the more patience we should have to end training
+                end_patience += 10
+                
                 step_norm_factor /= np.sqrt(10)
                 lr_reduce_wait = 0
 
