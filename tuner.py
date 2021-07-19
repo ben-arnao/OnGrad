a31e1ca5ce2e98e8a976bd92393f191301d24f12
Ben Arnao <arnaoben@gmail.com>
2021-07-19 13:33:33 -0400

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot
+import copy
 
 
 def train(
@@ -10,49 +11,31 @@ def train(
         get_episode_score,  # input is model, return a positive float for score
         model,
 
-        ### grad params ###
+        ### grad estimate params ###
+        momentum_base=0.99,
+        grad_est_min_delta_perc=0.03,
+        adaptive_mom_ratio=0.75,
+        mom_adaptive_factor=0.01,
+        grad_est_threshold=0.75,
+        noise_stddev=0.02,
 
-        noise_stddev=0.02,  # the standard deviation of noise used for estimating gradient
-        # values recommendations: near 0.1-0.2 (ex. 0.005 - 0.3)
-
-        init_est_samples=10,  # initial amount of samples used to calculating gradient for step
-        # values recommendations: 5, 10, 15+ (use larger values for more complex problems)
-
-        est_samples_reduce=1,  # amount to reduce est_samples by each LR reduce step
-        # values recommendations: 1-5. Increasing this can improve sample efficiency but may harm performance
-
-        grad_decay_factor=50,  # this can be seen as a form of momentum used in calculating gradients
-        # values recommendations: 1-1000 (larger value = lower momentum, more reactive estimate)
-
-        est_samples_floor=1,  # minimum amount of sample used to calculate gradient
-        # values recommendations: keep at default
-
-        grad_estimate_mode='additive',  # when using multiplicative, a reward increase of 10 -> 100 should be viewed
-        # similarly as a change of 100 -> 1000. (multiplicative only works with positive reward)
-        # values options: 'additive' or 'multiplicative'
-
-        ### patience params ###
-
-        patience=10,
-        patience_inc=10,
+        ### patience/reduce params ###
+        patience=20,
         min_delta=0.01,
-
-        lr_reduce_factor=3,  # factor to reduce average step size by (reduce LR)
-        # values recommendations: keep at default
+        lr_reduce_factor=2,
+        noise_reduce_factor=1,
 
         ### step/weight params ###
-
-        step_clip_factor=3,  # ensure that not step exceeds that standard dev of noise * X
+        step_clip_factor=1,  # ensure that not step exceeds that standard dev of noise * X
         # values recommendations: keep at default
 
-        init_lr=0.5,  # initial learn rate (factor)
+        init_lr=1,  # initial learn rate (factor)
         # values recommendations: 0.1 or 1
 
         weight_decay=0.1,  # weight decay *factor*. different from regular weight decay
         # values recommendations: 0.01 -> 1
 
         ### other ###
-
         init_iters=100  # if noise in weights is unable to produce a score difference after X attempts,
         # throw an error. For many problems, this should not be relevant. For problems where we expect the environment
         # to require many tries to generate varying scores, this can be increased.
@@ -76,9 +59,6 @@ def train(
         set_model_params(model, theta + step)
         score = get_episode_score(model)
         set_model_params(model, theta)
-
-        if score <= 0 and grad_estimate_mode == 'multiplicative':
-            raise ValueError('multiplicative grad estimate mode does not accept negative score values')
         return score
 
     def add_sample_to_grad_estimate(grad):
@@ -89,33 +69,30 @@ def train(
         pos_rew = get_step_score(model, v)
         neg_rew = get_step_score(model, -v)
 
-        # keep running total for gradient estimate
-        if pos_rew != neg_rew:
-            if grad_estimate_mode == 'multiplicative':
-                grad = np.where(pos_rew > neg_rew,
-                                grad + v * (pos_rew / neg_rew - 1),
-                                grad - v * (neg_rew / pos_rew - 1))
-            elif grad_estimate_mode == 'additive':
-                grad = np.where(pos_rew > neg_rew,
-                                grad + v * (pos_rew - neg_rew),
-                                grad - v * (neg_rew - pos_rew))
-            else:
-                raise ValueError('did not supply a valid option for \'grad_estimate_mode\'')
-        else:
-            print('Noise in weights did not produce a change in reward.')
+        # calculate momentum used to add current sample to estimate. static momentum is combined with adaptive momentum
+        # based on 'mom_adaptive_factor'
+        m = 2 - np.power(10, np.abs(v) * mom_adaptive_factor * noise_stddev)
+        m = np.where(m < 0, 0, m)
+        m = m * adaptive_mom_ratio + momentum_base * (1 - adaptive_mom_ratio)
 
-        return grad
+        # keep running total for gradient estimate
+        if pos_rew != neg_rew and pos_rew != 0 and neg_rew != 0:
+            if pos_rew > neg_rew:
+                grad = grad * m + np.sign(v) * (1 - m)
+            else:
+                grad = grad * m - np.sign(v) * (1 - m)
+
+        return grad, np.mean(m)
 
     # prep variables before training
     lr = init_lr
     iters_since_last_best = 0
     lr_reduce_wait = 0
-    est_samples = init_est_samples
 
     # ensure noise is able to produce varying scores
     i = 0
     while grad.any() == 0:
-        grad = add_sample_to_grad_estimate(grad)
+        grad, _ = add_sample_to_grad_estimate(grad)
         i += 1
         if i >= init_iters:
             raise Exception('Supplied model is not learnable as noise in weights was not able to produce varying '
@@ -128,9 +105,34 @@ def train(
 
     while True:
 
-        # accumlate samples for gradient estimate
-        for _ in range(est_samples):
-            grad = add_sample_to_grad_estimate(grad)
+        # estimate gradient for step
+        grad_hi = copy.deepcopy(grad)
+        grad_lo = copy.deepcopy(grad)
+
+        is_new_high = np.ones(grad.shape, dtype=bool)
+        is_new_low = np.ones(grad.shape, dtype=bool)
+
+        # here we keep accumulating samples and adding to estimate until 'grad_est_threshold' percent of gradient
+        # estimate stays within 'grad_min_delta_perc' of the current bounds
+
+        # in other words, we keep estimating gradient until the estimate is stationary enough
+        while sum(np.logical_not(np.logical_or(is_new_high, is_new_low))) / num_params < grad_est_threshold:
+
+            grad, m_avg = add_sample_to_grad_estimate(grad)
+
+            # momentum is scaled with noise magnitude. the smaller the noise, the larger the momentum and less impact
+            # this sample will have on the gradient estimate.
+            s = (1 - m_avg)
+
+            is_new_high = np.where(grad > 0,
+                                   np.where(grad > grad_hi * 1 + grad_est_min_delta_perc * s, True, False),
+                                   np.where(grad > grad_hi * 1 - grad_est_min_delta_perc * s, True, False))
+            grad_hi = np.where(is_new_high, grad, grad_hi)
+
+            is_new_low = np.where(grad < 0,
+                                  np.where(grad < grad_lo * 1 + grad_est_min_delta_perc * s, True, False),
+                                  np.where(grad < grad_lo * 1 - grad_est_min_delta_perc * s, True, False))
+            grad_lo = np.where(is_new_low, grad, grad_lo)
 
         # calculate step
         step = grad * ((noise_stddev * lr) / np.mean(np.abs(grad)))
@@ -140,12 +142,6 @@ def train(
 
         # calculate average step magnitude for use in determining weight decay factor
         step_mag = np.mean(np.absolute(step))
-
-        # decay gradient based on step size
-        grad_decay_perc = grad_decay_factor * step_mag
-        if grad_decay_perc > 1:
-            grad_decay_perc = 1
-        grad *= 1 - grad_decay_perc
 
         # get step score
         score = get_step_score(model, step)
@@ -173,10 +169,7 @@ def train(
             lr_reduce_wait += 1
 
             if lr_reduce_wait >= int(patience / 2):
-                if est_samples > est_samples_floor:
-                    est_samples -= est_samples_reduce
-
-                patience += patience_inc
+                noise_stddev /= noise_reduce_factor
                 lr /= lr_reduce_factor
                 lr_reduce_wait = 0
 
